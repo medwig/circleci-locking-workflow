99941aff56c5fd75bbf4b50ac1e898699b01676b
Jonathan Medwig <jonmedwig@gmail.com>
2019-09-17 16:30:04 -0400

    Use substring check

--- .circleci/acquire_lock.sh
+++ .circleci/acquire_lock.sh
@@ -13,17 +13,16 @@ then
 fi
 
 echo "A build is running. Check if it is this build..."
+
 echo $runningBuilds
+echo "build_num\":$CIRCLE_BUILD_NUM,"
 
-echo "build_num..$CIRCLE_BUILD_NUM,"
-isThisBuild=$(grep < $runningbuilds -e "build_num..$CIRCLE_BUILD_NUM,")
-
-echo $isThisBuild
-
-if test -z "$isThisBuild"
+if [[ $runningBuilds == *"build_num\":$CIRCLE_BUILD_NUM"* ]]
 then
+    echo "Only this build is running. Lock acquired"
+    exit 0
+else
     echo 'Another build is running. Wait for it to finish'
     exit 1
-fi
 
-echo "No other builds running. Lock acquired"
+fi
