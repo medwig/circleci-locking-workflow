aaed23e5d380c0046f5755772427d1cedf192a10
Jonathan Medwig <jonmedwig@gmail.com>
2019-09-17 15:59:36 -0400

    Bugfix the conditiona check to be length-based

--- .circleci/acquire_lock.sh
+++ .circleci/acquire_lock.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 CIRCLE_API='https://circleci.com/api/v1.1'
-NONE="[ ]"
 
 apiUrl="$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running"
-runningBuilds=$(curl -u $CIRCLE_TOKEN: "$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running")
+runningBuilds=$(curl -u $CIRCLE_TOKEN: -H "Accept: application/json" "$apiUrl")
 
-if [ "$runningbuilds" == "$none" ];
+# If no build is running the response will be "[]" (length=2)
+if [ ${#runningBuilds} -eq 2 ];
 then
     echo 'No builds running, lock acquired.'
     exit 0
 fi
 
 echo "A build is running. Check if it is this build..."
+echo $runningBuilds
 
-isThisBuild=$(echo $runningbuilds | grep "build_num\" : $CIRCLE_BUILD_NUM")
+isThisBuild=$(echo $runningbuilds | grep "build_num\":$CIRCLE_BUILD_NUM")
 if [ -z "$isThisBuild" ];
 then
     echo 'Another build is running. Wait for it to finish'
-    echo $runningBuilds
     exit 1
 fi
 
