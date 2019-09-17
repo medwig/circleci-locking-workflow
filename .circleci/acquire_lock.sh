a527f7dd85b4a04e54771decb8f168f5687c8792
Jonathan Medwig <jonmedwig@gmail.com>
2019-09-17 14:45:23 -0400

    Fix conditional check for running builds

--- .circleci/acquire_lock.sh
+++ .circleci/acquire_lock.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-apiUrl = "$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running"
-runningBuilds = $(curl -u $CIRCLE_TOKEN: "$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running")
+CIRCLE_API='https://circleci.com/api/v1.1'
+NONE="[ ]"
 
-if [ -z "$runningBuilds" ]
+apiUrl="$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running"
+runningBuilds=$(curl -u $CIRCLE_TOKEN: "$CIRCLE_API/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME?shallow=true&limit=1&filter=running")
+
+if [ "$runningBuilds" == "$NONE" ];
 then
-      echo "No running builds, lock acquired"
-      return 0
+    echo 'No builds running, lock acquired.'
 else
-      echo "A build is running, waiting for it to finish"
-      return 1
+    echo 'A build is running, wait for it to finish'
+    echo $runningBuilds
 fi
